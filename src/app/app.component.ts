eb878bfa1bbd819642d604763c89d2b4a3a0df5b
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2021-01-02 15:40:28 -0800

    App: add board creation functionality, add list of dies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {DIES} from '../assets/constants';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Boggle';
+  board: string[][] = [];
+
+  ngOnInit(): void{
+    this.shakeBoard();
+  }
+
+  shakeBoard(): void{
+    console.log('create board called');
+    let diesCopy = JSON.parse(JSON.stringify(DIES));
+    const	board: string[][] = [];
+    for (let row = 0; row < 4; row++){
+      const r = [];
+      for (let col = 0; col < 4; col++){
+        const die = Math.floor(Math.random() * diesCopy.length);
+        const face = Math.floor(Math.random() * 6);
+        const faceValue = diesCopy[die][face];
+        r.push(faceValue);
+        diesCopy = diesCopy.slice(0, die).concat(diesCopy.slice(die + 1));
+      }
+      board.push(r);
+    }
+    this.board = board;
+    console.log('board is', this.board);
+  }
 }
