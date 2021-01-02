c43dee2ca0f6302db5170e0586f990a15a4794c1
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2021-01-02 14:50:09 -0800

    Board: create Board as stub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardModule } from './board/board.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    BoardModule,
     BrowserModule
   ],
   providers: [],
