ad0c730fe067fc8f27e21772d6b26c25d853a140
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2021-01-02 21:11:08 -0800

    Auto-generated commit

---
+++ assets/constants.ts
@@ -0,0 +1,25 @@
+export const DIES = [
+  ['A', 'C', 'D', 'E', 'P', 'M'],
+  ['G', 'I', 'V', 'E', 'N', 'T'],
+  ['V', 'E', 'N', 'D', 'A', 'Z'],
+  ['Qu', 'J', 'A', 'M', 'O', 'B'],
+  ['R', 'A', 'H', 'M', 'O', 'S'],
+  ['F', 'E', 'E', 'H', 'I', 'Y'],
+  ['D', 'O', 'W', 'S', 'N', 'E'],
+  ['P', 'I', 'S', 'N', 'E', 'H'],
+  ['S', 'E', 'P', 'L', 'U', 'T'],
+  ['T', 'A', 'L', 'B', 'I', 'Y'],
+  ['B', 'I', 'R', 'X', 'O', 'F'],
+  ['O', 'K', 'N', 'T', 'U', 'D'],
+  ['G', 'L', 'U', 'W', 'I', 'R'],
+  ['U', 'K', 'G', 'E', 'L', 'Y'],
+  ['L', 'E', 'S', 'R', 'A', 'C'],
+  ['O', 'A', 'T', 'C', 'I', 'A'],
+];
+
+export enum TimerStatus {
+  WAITING,
+  RUNNING,
+  NEAR_END,
+  EXPIRED,
+}
